7652dc3aa41ffbb7aaccbc636adda89433c967b6
Alexis Marc Boisset Pérez <alexismarcbp@gmail.com>
2024-11-19 01:16:34 +0100

    Testing

--- controller/session/session.controller.php
+++ controller/session/session.controller.php
@@ -5,19 +5,6 @@
 
 session_start();
 
-// Obtener la ruta relativa de la carpeta raíz del proyecto
-$projectRoot = realpath(__DIR__ . '/../'); // Ajusta según la profundidad de la carpeta raíz
-
-// Convierte la ruta a URL para que funcione con el servidor actual
-$projectRootUrl = 'http://' . $_SERVER['HTTP_HOST'] . str_replace(
-    $_SERVER['DOCUMENT_ROOT'],
-    '',
-    $projectRoot
-);
-
-// Define la URL base del proyecto
-define('BASE_URL', rtrim($projectRootUrl, '/'));
-
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 2400) && ($_SESSION['loggedin'])) {
     // Si han pasado más de 40 minutos
     session_unset();
@@ -25,7 +12,7 @@ if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 
     session_start(); // Vuelvo a abrir la session para tener feedback de sesion expirada
     $_SESSION['failure'] = "Sessió expirada";
 
-    header("Location: " . BASE_URL . "/view/login.view.php"); // Redirigir al login
+    header("Location: " . BASE_URL . "/views/auth/login/login.view.php"); // Redirigir al login
     exit();
 } elseif ($_SESSION['loggedin'] == true) {
     $_SESSION['LAST_ACTIVITY'] = time(); // Actualizar el tiempo de última actividad
