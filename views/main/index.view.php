a711475132c1c3c82d46cddf667297bb16a780bb
Alexis Marc Boisset Pérez <alexismarcbp@gmail.com>
2024-11-19 01:05:19 +0100

    Fixing style

--- views/main/index.view.php
+++ views/main/index.view.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestor de partits</title>
-    <link rel="stylesheet" href="<?php echo __DIR__ . '/'?>styles.css">
+    <link rel="stylesheet" href="<?php echo BASE_PATH . '/'?>styles.css">
 </head>
 
 <header>
