f98e2d94bd77df2156fead9a5374dad725e61077
Alexis Marc Boisset Pérez <alexismarcbp@gmail.com>
2024-11-20 15:38:36 +0100

    Eliminar barra diagonal innecesaria en la ruta de inicio de sesión y ajustar la manipulación de la URI en index.php

--- views/main/index.view.php
+++ views/main/index.view.php
@@ -16,7 +16,7 @@
         <a href="<?php echo BASE_URL; ?>/logout" class="btn-logout">Tancar sessió</a>
     <?php else: ?>
         <!-- Opciones de logarse o registrar-se cuando no está logado -->
-        <a href="<?php echo BASE_URL; ?>login" class="btn-login">Logar-se</a>
+        <a href="<?php echo BASE_URL; ?>/login" class="btn-login">Logar-se</a>
         <a href="<?php echo BASE_URL; ?>/register" class="btn-register">Enregistrar-se</a>
     <?php endif; ?>
 </header>
